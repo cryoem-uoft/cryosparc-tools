0787e5e59efa0188f20a7b8c782f6f9c991cd5a7
Nick Frasser <nfrasser@structura.bio>
2022-07-04 18:25:07 -0400

    test: mock command_core server
    Using httpretty

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,53 @@
+import json
 from pathlib import Path
+import shutil
+import urllib.request
 import pytest
+import httpretty
 from cryosparc.dataset import Dataset
 
-@pytest.fixture(scope='session')
-def big_dset():
-    basename = 'bench_big_dset'
-    existing_path = Path.home() / f'{basename}.cs'
+
+def request_callback(request, uri, response_headers):
+    procs = ["hello_world"]  # Possible methods list
+    body = json.loads(request.body)
+
+    res = None
+    if body["method"] == "system.describe":
+        procs = [{"name": m} for m in procs]
+        res = {"procs": procs}
+    elif body["method"] == "hello_world":
+        res = {"hello": "world"}
+
+    response_headers["content-type"] = "application/json"
+    return [200, response_headers, json.dumps({"result": res})]
+
+
+@pytest.fixture(scope="session")
+def big_dset(tmpdir):
+    basename = "bench_big_dset"
+    existing_path = Path.home() / f"{basename}.cs"
 
     if existing_path.exists():
         yield Dataset.load(existing_path)
     else:
-        # FIXME: Download test data from from S3
-        import requests, tarfile, tempfile
-        with tempfile.TemporaryDirectory() as tmpdir:
-            download_url = f'https://cryosparc-test-data-dist.s3.amazonaws.com/{basename}.tar.gz'
-            download_checksum_url = f'{download_url}.sha256'
-            tar_path = f'{tmpdir}/{basename}.tar.gz'
-            cs_path = f'{tmpdir}/{basename}.cs'
+        import gzip
 
-            req = requests.get(download_checksum_url)
-            req.raise_for_status()
-            # checksum = req.text.strip()
-            # util.download_and_verify_url(download_url, tar_path, checksum)
-            with tarfile.open(tar_path, "r:gz") as tar:
-                tar.extractall(tmpdir)
+        download_url = (
+            f"https://cryosparc-test-data-dist.s3.amazonaws.com/{basename}.cs.gz"
+        )
+        compressed_path = Path(tmpdir) / f"{basename}.cs.gz"
+        urllib.request.urlretrieve(download_url, compressed_path)
+        cs_path = Path(tmpdir) / f"{basename}.cs"
+        with gzip.open(compressed_path, "rb") as compressed_file:
+            with open(cs_path, "wb") as cs_file:
+                shutil.copyfileobj(compressed_file, cs_file)
 
-            yield Dataset.load(cs_path)
+        yield Dataset.load(cs_path)
+
+
+@pytest.fixture(scope="session")
+def command_core():
+    httpretty.enable(allow_net_connect=False)
+    httpretty.register_uri(httpretty.POST, "http://localhost:39002/api", body=request_callback)  # type: ignore
+    yield
+    httpretty.disable()
