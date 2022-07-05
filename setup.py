099430053ae0f83aaef4f1e960c9c5f4f339e33e
Nick Frasser <nfrasser@structura.bio>
2022-07-05 17:29:58 -0400

    refactor: improved dataset ext module spec
    Use dedicated properties instead of compiled flags, specify headers file

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import os
-import sysconfig
 import numpy
 from setuptools import Extension, setup
 
@@ -8,13 +6,15 @@ setup(
     name="cryosparc_tools",
     version="0.1.0",
     description="Toolkit for interfacing with cryoSPARC",
+    headers=["src/dataset.h"],
     ext_modules=[
         Extension(
             name="cryosparc_dataset",
-            sources=["src/pywrapper_module.c", "src/pywrapper_dataset.c"],
+            libraries=['pthread', 'm'],
             include_dirs=[numpy.get_include()],
-            extra_compile_args=["-DMODULENAME=cryosparc_dataset"],
-            extra_link_args=["-lpthread", "-lm"],
+            define_macros=[('MODULENAME', 'cryosparc_dataset')],
+            sources=["src/pywrapper_module.c", "src/pywrapper_dataset.c"],
+            depends=["src/dataset.h", "src/pywrapper_wrapperfunctions.c"],
         )
     ],
 )
