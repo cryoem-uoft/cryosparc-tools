a8fe1647e1da233e0d26ca61d8611e63cdfe4f87
Nick Frasser <nfrasser@structura.bio>
2022-07-15 16:30:17 -0400

    test: fix big_dset download conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 import shutil
+import tempfile
+from time import time
 import urllib.request
 import pytest
 import httpretty
@@ -23,26 +25,43 @@ def request_callback(request, uri, response_headers):
 
 
 @pytest.fixture(scope="session")
-def big_dset(tmpdir):
+def big_dset():
     basename = "bench_big_dset"
     existing_path = Path.home() / f"{basename}.cs"
 
+    print("")  # Keeps printing clean
     if existing_path.exists():
-        yield Dataset.load(existing_path)
+        print(f"Using local sample data {existing_path}; loading...", end="\r")
+        tic = time()
+
+        dset = Dataset.load(existing_path)
+        print(f"Using local sample data {existing_path}; loaded in {time() - tic:.3f} seconds")
+        yield dset
     else:
         import gzip
+        import tempfile
 
-        download_url = (
-            f"https://cryosparc-test-data-dist.s3.amazonaws.com/{basename}.cs.gz"
-        )
-        compressed_path = Path(tmpdir) / f"{basename}.cs.gz"
-        urllib.request.urlretrieve(download_url, compressed_path)
-        cs_path = Path(tmpdir) / f"{basename}.cs"
-        with gzip.open(compressed_path, "rb") as compressed_file:
-            with open(cs_path, "wb") as cs_file:
-                shutil.copyfileobj(compressed_file, cs_file)
+        download_url = f"https://cryosparc-test-data-dist.s3.amazonaws.com/{basename}.cs.gz"
+        with tempfile.TemporaryDirectory() as tmpdir:
+            compressed_path = Path(tmpdir) / f"{basename}.cs.gz"
+            cs_path = Path(tmpdir) / f"{basename}.cs"
 
-        yield Dataset.load(cs_path)
+            def download_report_hook(chunk, chunk_size, total_size):
+                total_chunks = total_size / chunk_size
+                progress = chunk / total_chunks * 100
+                print(f"Downloading big dataset sample data ({total_size} bytes, {progress:.0f}%)", end='\r')
+
+            urllib.request.urlretrieve(download_url, compressed_path, reporthook=download_report_hook)
+            with gzip.open(compressed_path, "rb") as compressed_file:
+                with open(cs_path, "wb") as cs_file:
+                    shutil.copyfileobj(compressed_file, cs_file)
+
+            print("")
+            print("Downloaded big dataset sample data; loading...", end='\r')
+            tic = time()
+            dset = Dataset.load(cs_path)
+            print(f"Downloaded big dataset sample data; loaded in {time() - tic:.3f} seconds")
+            yield dset
 
 
 @pytest.fixture(scope="session")
