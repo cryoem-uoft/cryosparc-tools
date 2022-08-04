d033dff46fe8249f6b0a7851bb1a35a1030921cd
Nick Frasser <nfrasser@structura.bio>
2022-08-04 19:00:04 -0400

    feat: project, workspace and job document specs

--- cryosparc/spec.py
+++ cryosparc/spec.py
@@ -1,16 +1,11 @@
-from enum import Enum
-from typing import NamedTuple
+from typing import List, Optional, Tuple, Union
+from typing_extensions import Literal, TypedDict
 
 
-class Datatype(str, Enum):
-    exposure = "exposure"
-    particle = "particle"
-    template = "template"
-    volume = "volume"
-    mask = "mask"
+Datatype = Literal["exposure", "particle", "template", "volume", "mask"]
 
 
-class Datafield(NamedTuple):
+class Datafield(TypedDict):
     """Definition of a prefix field within a CS file."""
 
     dtype: str
@@ -24,3 +19,107 @@ class Datafield(NamedTuple):
     required: bool
     """whether this field must necessarily exist in a corresponding
     input/output"""
+
+
+class InputSlot(TypedDict):
+    type: str
+    name: str
+    title: str
+    description: str
+    optional: bool
+
+
+class ConnectionSlot(TypedDict):
+    slot_name: Optional[str]  # passthrough slots have slot_name None
+    job_uid: str
+    group_name: str  # e.g., particles
+    result_name: str  # e.g., blob
+    result_type: str  # e.g., particle.blob
+    version: Union[int, Literal["F"]]
+
+
+class Connection(TypedDict):
+    job_uid: str
+    group_name: str
+    slots: List[ConnectionSlot]
+
+
+class InputSlotGroup(TypedDict):
+    type: Datatype
+    name: str
+    title: str
+    description: str
+    count_min: int
+    count_max: Optional[int]
+    repeat_allowed: bool
+    slots: List[InputSlot]
+    connections: List[Connection]
+
+
+class OutputResultGroupContains(TypedDict):
+    uid: str
+    type: str  # e.g., particle.alignments3D
+    group_name: str  # e.g., particles
+    name: str  # e.g., alignments_class_1
+    passthrough: bool
+
+
+class OutputResultGroup(TypedDict):
+    uid: str
+    type: Datatype
+    name: str
+    title: str
+    description: str
+    contains: List[OutputResultGroupContains]
+    passthrough: Union[str, Literal[False]]
+    num_items: int
+    summary: dict
+
+
+class OutputResult(TypedDict):
+    uid: str
+    type: str
+    name: str
+    group_name: str
+    title: str
+    description: str
+    versions: List[int]
+    metafiles: List[str]
+    min_fields: List[Tuple[str, str]]
+    num_items: int
+    passthrough: bool
+
+
+class JobDocument(TypedDict):
+    _id: str
+    uid: str
+    uid_num: int
+    project_uid: str
+    project_uid_num: int
+    job_type: str
+    title: str
+    description: str
+    status: str
+    created_at: str
+    created_by_user_id: Optional[str]
+    deleted: bool
+    parents: List[str]
+    children: List[str]
+    input_slot_groups: List[InputSlotGroup]
+    output_result_groups: List[OutputResultGroup]
+    output_results: List[OutputResult]
+    workspace_uids: List[str]
+
+
+class WorkspaceDocument(TypedDict):
+    _id: str
+    uid: str
+    uid_num: int
+    project_uid: str
+    project_uid_num: int
+    created_at: str
+    created_by_user_id: str
+    deleted: bool
+    title: str
+    description: Optional[str]
+    workspace_type: Literal["base", "live"]
