8edddcdc8cd1de5f49d32225240aab6a1c58d16b
Nick Frasser <nfrasser@structura.bio>
2022-10-25 19:19:37 -0400

    test: add innerjoin C tests

--- src/test.c
+++ src/test.c
@@ -22,18 +22,22 @@ int main (int argc, char ** argv)
 	printf("nrow %u \n", dset_nrow(d));
 
 	xassert(dset_addrows(d, 10));
+	xassert(dset_addcol_scalar(d, "uid", T_U64));
 	xassert(dset_addcol_scalar(d, "col1", T_F32));
 	xassert(dset_addcol_scalar(d, "col2", T_F32));
 	xassert(dset_addcol_scalar(d, "col3", T_F32));
 
+	uint64_t * uid = dset_get(d, "uid");
 	float * col1 = dset_get(d, "col1");
 	float * col2 = dset_get(d, "col2");
 	float * col3 = dset_get(d, "col3");
 
-	#define LONGSTR "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery long string as column header"
+	// #define LONGSTR "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery long string as column header"
+	#define LONGSTR "very"
 	xassert(dset_addcol_scalar(d, LONGSTR, T_STR));
 
 	for (int i = 0; i < 10; i++) {
+		uid[i] = (i + 1) * 10;
 		col1[i] = sinf((float)i);
 		col2[i] = cosf((float)i);
 		col3[i] = tanf((float)i);
@@ -53,9 +57,33 @@ int main (int argc, char ** argv)
 	dset_dumptxt(d);
 	printf("\n");
 
-	dset_del(d);
+	uint64_t e = dset_new();
+	xassert(dset_addrows(e, 7));
+	xassert(dset_addcol_scalar(e, "uid", T_U64));
+	xassert(dset_addcol_scalar(e, "col4", T_F32))
+	xassert(dset_addcol_scalar(e, "morestrs", T_STR))
 
-	// Hashtable tests
+	uid = dset_get(e, "uid");
+	float * col4 = dset_get(e, "col4");
+
+	for (int i = 0; i < 7; i++) {
+		uid[i] = (7 - i) * 10 + i % 2;
+		col4[i] = sinf((float)i);
+		xassert(dset_setstr(e, "morestrs", i, randstr()));
+	}
+
+	dset_dumptxt(e);
+	printf("\n");
+	uint64_t f = dset_innerjoin("uid", d, e);
+	dset_dumptxt(f);
+	printf("\n");
+
+	dset_del(d);
+	dset_del(e);
+	dset_del(f);
+
+	/*
+	// Hash table tests
 	uint64_t val;
 	ht64 ht;
 	ht64_new(&ht, 3);
@@ -78,4 +106,5 @@ int main (int argc, char ** argv)
 
 	// Deallocate
 	ht64_del(&ht);
+	*/
 }
