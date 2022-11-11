8aca896b69f256118828ce3bde767bb57eff8976
Nick Frasser <nfrasser@structura.bio>
2022-11-11 17:22:03 -0500

    v4.0.1
    Includes some bug fixes for developer testing

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ elif DEBUG:
 
 setup(
     name="cryosparc_tools",
-    version="4.0.0",
+    version="4.0.1",
     description="Toolkit for interfacing with CryoSPARC",
     headers=["src/dataset.h"],
     package_data={"cryosparc": ["src/dataset.h", "cryosparc/core.pyx"]},
