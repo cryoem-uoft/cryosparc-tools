a7e98d1131091d10b30664a380473426e99c907e
Nick Frasser <nfrasser@structura.bio>
2022-12-12 10:55:13 -0500

    v4.1.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ elif DEBUG:
 
 setup(
     name="cryosparc_tools",
-    version="0.0.3",
+    version="4.1.0",
     description="Toolkit for interfacing with CryoSPARC",
     headers=["cryosparc/include/cryosparc-tools/dataset.h"],
     ext_modules=cythonize(
