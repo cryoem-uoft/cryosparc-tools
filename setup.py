e3f6acf0bbd22d8147f7af702f79981f15da0900
Nick Frasser <1693461+nfrasser@users.noreply.github.com>
2023-12-05 10:28:01 -0500

    v4.4.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ elif DEBUG:
 
 setup(
     name="cryosparc_tools",
-    version="4.4.0",
+    version="4.4.1",
     description="Toolkit for interfacing with CryoSPARC",
     headers=["cryosparc/include/cryosparc-tools/dataset.h"],
     ext_modules=cythonize(
