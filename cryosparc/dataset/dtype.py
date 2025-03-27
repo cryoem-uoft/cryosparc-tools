1c392f3f608db5cfb99fac42d2b34fd317c6cb00
nwong <nwong@structura.bio>
2025-03-27 10:19:17 -0400

    fix test cs client args when env present, improve dtype types map

--- cryosparc/dataset/dtype.py
+++ cryosparc/dataset/dtype.py
@@ -35,7 +35,7 @@ class DatasetHeader(TypedDict):
     """Field names that require decompression."""
 
 
-DSET_TO_TYPE_MAP: Dict[DsetType, Type] = {
+DSET_TO_TYPE_MAP: Dict[DsetType, Type[n.number | n.object_]] = {
     DsetType.T_F32: n.float32,
     DsetType.T_F64: n.float64,
     DsetType.T_C32: n.complex64,
@@ -111,7 +111,7 @@ def get_data_field_dtype(data: Data, field: str) -> "DTypeLike":
         raise KeyError(f"Unknown dataset field {field}")
     elif t not in DSET_TO_TYPE_MAP:
         raise KeyError(f"Unknown dataset field type {t}")
-    dt = n.dtype(DSET_TO_TYPE_MAP[t])  # type: ignore
+    dt = n.dtype(DSET_TO_TYPE_MAP[t])
     shape = data.getshp(field)
     return (dt.str, shape) if shape else dt.str
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -255,7 +255,13 @@ def mock_api_client_class(mock_user, monkeypatch):
 
 @pytest.fixture
 def cs(mock_api_client_class):
-    return CryoSPARC("https://cryosparc.example.com", email="structura@example.com", password="password")
+    return CryoSPARC(
+        "https://cryosparc.example.com",
+        email="structura@example.com",
+        password="password",
+        host=None,
+        base_port=None,
+    )
 
 
 @pytest.fixture
